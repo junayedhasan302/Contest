a1d8ac64d6590844fdf094d38514467b2729f8b3
junayedhasan302 <junayedhasan302@gmail.com>
2026-08-25 02:53:46 +0600

    P2 Count Number Properties

--- DContest-1/p2countNumberProperties.ts
+++ DContest-1/p2countNumberProperties.ts
@@ -21,7 +21,6 @@ let even = 0, odd = 0, positive = 0, negative = 0;
   return {even,odd,positive,negative};
 }
 
-
 // Input
 const numbers: number[] = [-5, 0, 3, -4, 1];
 // Output --> { even: 2, odd: 3, positive: 2, negative: 2 }
