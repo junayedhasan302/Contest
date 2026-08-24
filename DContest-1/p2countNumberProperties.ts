8473416b13f37748fd208d8d128518ffc3b8f7c0
junayedhasan302 <junayedhasan302@gmail.com>
2026-08-25 02:21:18 +0600

    Count Number Properties

---
+++ DContest-1/p2countNumberProperties.ts
@@ -0,0 +1,28 @@
+// JUNAYED HASAN
+
+interface ReturnedObj {
+  even: number;
+  odd: number;
+  positive: number;
+  negative: number;
+}
+
+function countNumberProperties(numbers: number[]): ReturnedObj {
+let even = 0, odd = 0, positive = 0, negative = 0;
+  for (const num of numbers) {
+    // EVEN ODD
+    if (num % 2 === 0) {even++;}
+    else {odd++;}
+
+    // POS NEG
+    if (num > 0) {positive++;}
+    else if (num < 0) {negative++;}
+  }
+  return {even,odd,positive,negative};
+}
+
+
+// Input
+const numbers: number[] = [-5, 0, 3, -4, 1];
+// Output --> { even: 2, odd: 3, positive: 2, negative: 2 }
+console.log(countNumberProperties(numbers));
